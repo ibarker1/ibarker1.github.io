5d3976e30ad748939759e86c0cb782b483600cc2
Isaiah <ibarker1@binghamton.edu>
2018-03-13 17:44:03 -0400

    Email List
    Updated php for email function.

--- php/mailscript.php
+++ php/mailscript.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['submit'])) {
 
-	$to = 'your.website@site.com'; // PUT YOUR EMAIL ADRESS INSTEAD your.website@site.com
+	$to = 'isaiah@ijbarker.com'; // PUT YOUR EMAIL ADRESS INSTEAD your.website@site.com
 	$subject = $_POST['subject'];
 	$message = '<b>Email from your website.</b><br><br>
 				First name: '.$_POST['first_name'].'<br>'.'
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])) {
 				Email: '.$_POST['email'].'<br>'.'
 				Message: '.$_POST['massage'].'<br>';
 	$mheaders  = "Content-type: text/html; charset=utf-8 \r\n"; 
-	$mheaders .= "From: Your website <your.website@site.com>\r\n"; 
+	$mheaders .= "From: Your website <isaiah@ijbarker.com>\r\n"; 
 	mail($to, $subject, $message, $mheaders);
 	header("Location: ../emailsent.html");
 
